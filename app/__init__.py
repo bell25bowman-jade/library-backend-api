c71d834ac360d3d3b0f8c004326832002aa7bc59
bell25bowman-jade <bell25bowman@gmail.com>
2026-06-22 01:47:26 -0400

    upload workflow github actions

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,19 @@ from .blueprints.members import members_bp
 from .blueprints.book import book_bp
 from .blueprints.loan import loan_bp
 from .extensions import ma, limiter, cache
+from flask_swagger_ui import get_swaggerui_blueprint
+
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/swagger.yaml'  # Our API URL (can of course be a local resource)
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Your API's Name"
+    }
+)
+
 
 def create_app(config_name: str):
     app = Flask(__name__)
@@ -19,6 +32,9 @@ def create_app(config_name: str):
     app.register_blueprint(members_bp, url_prefix="/members")
     app.register_blueprint(book_bp, url_prefix="/books")
     app.register_blueprint(loan_bp, url_prefix="/loans")
+     app.register_blueprint(items_bp, url_prefix="/items")
+    app.register_blueprint(orders_bp, url_prefix="/orders")
+		app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL) #Registering our swagger blueprint
     
     
     return app
